8ec0353df8c20b8643dd83620322d30209b2c2a4
kaamila42 <77609265+kaamila42@users.noreply.github.com>
2021-01-18 02:17:11 -0600

    Add files via upload

---
+++ PROJECT A.py
@@ -0,0 +1,7 @@
+# Program to calculate area of a circle of a given radius
+from math import pi
+RADIUS = float(input(" ENTER THE RADIUS OF THE CIRCLE: "))
+area = pi*RADIUS*RADIUS;
+print(" THE AREA OF CIRCLE WITH THE RADIUS: ", format(area,'.16f'),"sq units")
+
+              
---
+++ project1b.py
@@ -0,0 +1,3 @@
+filename = input("Input the Filename:  " ,"py = python", "txt = text", ...)
+f_extns = filename.split(".")
+print(f " The extension of the file is '{f_extns[]}'")
